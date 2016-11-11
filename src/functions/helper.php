3b882de5b6804d16daaa1b756450a1ae64a3c348
李铭昕 <715557344@qq.com>
2016-11-11 12:00:10 +0800

    * 新增env函数

--- src/functions/helper.php
+++ src/functions/helper.php
@@ -49,3 +49,38 @@ if (!function_exists('session')) {
         return $session->set($key, $value);
     }
 }
+
+if (!file_exists('env')) {
+    /**
+     * Gets the value of an environment variable. Supports boolean, empty and null.
+     *
+     * @param  string $key
+     * @param  mixed $default
+     * @return mixed
+     */
+    function env($key, $default = null)
+    {
+        $value = getenv($key);
+
+        if ($value === false) {
+            return $default;
+        }
+
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+            case 'false':
+            case '(false)':
+                return false;
+            case 'empty':
+            case '(empty)':
+                return '';
+            case 'null':
+            case '(null)':
+                return;
+        }
+
+        return $value;
+    }
+}
